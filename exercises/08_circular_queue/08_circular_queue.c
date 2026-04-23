618033771969d3891effa681a243c7d53d739863
jensenojs <jensenojs@qq.com>
2026-04-23 23:44:29 +0800

    08

--- exercises/08_circular_queue/08_circular_queue.c
+++ exercises/08_circular_queue/08_circular_queue.c
@@ -14,14 +14,47 @@ typedef struct {
     int count;
 } Queue;
 
+void in(Queue *q, int id) {
+    if (q->count == MAX_PEOPLE) return;
+    q->data[q->tail].id = id;
+    q->tail = (q->tail + 1) % MAX_PEOPLE;
+    q->count++;
+}
+
+int out(Queue *q) {
+    if (q->count == 0) return -1;
+    int id = q->data[q->head].id;
+    q->head = (q->head + 1) % MAX_PEOPLE;
+    q->count--;
+    return id;
+}
+
 int main() {
     Queue q;
     int total_people=50;
     int report_interval=5;
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    
+    // init
+    q.head = 0;
+    q.tail = 0;
+    q.count = 0;
+
+    for(int i = 0; i < MAX_PEOPLE; i++) {
+        in(&q, i+1);
+    }
+
+    while(total_people > 1) {
+        int id = -1;
+        for(int i = 0; i < report_interval - 1; i++) {
+            id = out(&q);
+            if(id != -1) in(&q, id);
+        }
+        id = out(&q);
+        printf("淘汰: %d\n", id);
+
+        total_people--;
+    }
+
     printf("最后剩下的人是: %d\n", q.data[q.head].id);
 
     return 0;
