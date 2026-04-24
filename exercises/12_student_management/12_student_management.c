f786856e39ee9b69835bfc942c6d45c76409075b
jensenojs <jensenojs@qq.com>
2026-04-24 10:40:42 +0800

    12

--- exercises/12_student_management/12_student_management.c
+++ exercises/12_student_management/12_student_management.c
@@ -19,8 +19,17 @@ int main() {
     
     for (int i = 0; i < 3; i++) 
     {
-	    // TODO: 在这里添加你的代码
-        // I AM NOT DONE
+	    students[i] = (Student *)malloc(sizeof(Student));
+        if (students[i] == NULL) {
+            fclose(file);
+            for (int j = 0; j < i; j++) {
+                free(students[j]);
+            }
+            printf("内存分配失败\n");
+            return 1;
+        }
+
+        fscanf(file, "%19s %49s %d", students[i]->id, students[i]->name, &students[i]->age);
     }
     fclose(file);
     
