9edad4ca2438c00c5e579c2309ca77e51353fcfc
jensenojs <jensenojs@qq.com>
2026-04-24 10:25:29 +0800

    10

--- exercises/10_my_strcpy/10_my_strcpy.c
+++ exercises/10_my_strcpy/10_my_strcpy.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+void my_strcpy(char *dest, const char *src) {
+    while (*src != '\0') {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+}
+
 int main() {
     char source[] = "Hello, World! This is a test string for my_strcpy function.\n";
     char destination[101];
-    
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    
+
+    my_strcpy(destination, source);
+
     printf("拷贝后的字符串: %s", destination);
-    
+
     return 0;
 }
