9189bacfcaba85ce7af5403cbcc4b1ff0240182e
jensenojs <jensenojs@qq.com>
2026-04-24 10:37:42 +0800

    11

--- exercises/11_command_interpreter/11_command_interpreter.c
+++ exercises/11_command_interpreter/11_command_interpreter.c
@@ -57,11 +57,19 @@ int shell_parse(char *buf, char *argv[])
 {
     int argc = 0;
     int state = 0;
-    // TODO: 在这里添加你的代码，完成命令行解析
-    // 功能：将输入字符串buf按空格分割成多个参数，存入argv数组
-    // 返回：参数个数argc
-    // 提示：使用状态机的方式处理，注意处理字符串结束符
-    // I AM NOT DONE
+
+    for (char *p = buf; *p != '\0'; p++) {
+        if (*p == ' ' || *p == '\t') {
+            *p = '\0';
+            state = 0;
+        } else if (state == 0) {
+            if (argc < MAX_ARGS) {
+                argv[argc++] = p;
+            }
+            state = 1;
+        }
+    }
+
     return argc;
 }
 
