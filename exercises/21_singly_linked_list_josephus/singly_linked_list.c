1eb36267638ad31ed6f4787f6be550bed4ffe1d3
jensenojs <jensenojs@qq.com>
2026-04-27 17:51:41 +0800

    21

--- exercises/21_singly_linked_list_josephus/singly_linked_list.c
+++ exercises/21_singly_linked_list_josephus/singly_linked_list.c
@@ -8,8 +8,11 @@ static link head = NULL;
 
 // 创建新节点
 link make_node(unsigned char item) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    link node = (link)malloc(sizeof(struct node));
+    if (node == NULL) return NULL;
+    node->item = item;
+    node->next = NULL;
+    return node;
 }
 
 // 释放节点
@@ -17,48 +20,76 @@ void free_node(link p) { free(p); }
 
 // 查找节点
 link search(unsigned char key) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    link curr = head;
+    while (curr != NULL) {
+        if (curr->item == key) {
+            return curr;
+        }
+        curr = curr->next;
+    }
+    return NULL;
 }
 
 // 在链表头部插入节点
 void insert(link p) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    p->next = head;
+    head = p;
 }
 
 // 删除指定节点
 void delete(link p) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (head == p) {
+        head = head->next;
+        return;
+    }
+    link curr = head;
+    while (curr != NULL && curr->next != p) {
+        curr = curr->next;
+    }
+    if (curr != NULL) {
+        curr->next = p->next;
+    }
 }
 
 // 遍历链表
 void traverse(void (*visit)(link)) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    link curr = head;
+    while (curr != NULL) {
+        visit(curr);
+        curr = curr->next;
+    }
 }
 
 // 销毁整个链表
 void destroy(void) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    link curr = head;
+    while (curr != NULL) {
+        link next = curr->next;
+        free_node(curr);
+        curr = next;
+    }
+    head = NULL;
 }
 
 // 在链表头部推入节点
-void push(link p) { 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
- }
+void push(link p) {
+    insert(p);
+}
 
 // 从链表头部弹出节点
 link pop(void) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (head == NULL) return NULL;
+    link temp = head;
+    head = head->next;
+    return temp;
 }
 
 // 释放链表内存
 void free_list(link list_head) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    link curr = list_head;
+    while (curr != NULL) {
+        link next = curr->next;
+        free_node(curr);
+        curr = next;
+    }
 }
